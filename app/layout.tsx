ce92e68316efaec6a427ac736738a705dfaabccc
42Colins <colinproj@icloud.com>
2025-02-21 16:01:37 +0100

    Logo changed to blank image

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,6 @@ const inter = Inter({ subsets: ['latin'], weight: ['300', '400', '500'] });
 export const metadata: Metadata = {
   title: 'g-archives.fr',
   description: 'Visual Artist & Photographer Portfolio',
-  icons: {
-    icon: '/Blank.png', // Default favicon
-    shortcut: '/Blank.png'
-  },
 };
 
 export default function RootLayout({
@@ -20,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+          <link rel="icon" href="/icon.ico" type="image/ico" />
       <body className={inter.className}>{children}</body>
     </html>
   );
